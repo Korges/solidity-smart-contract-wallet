4d50602b3d66223ef91d925b5e9e9fc123968c4c
korges <korgesik@gmail.com>
2025-10-30 12:04:40 +0100

    guardian

--- SmartContractWallet.sol
+++ SmartContractWallet.sol
@@ -9,8 +9,35 @@ contract SmartContractWallet {
     mapping(address => uint) public allowance;
     mapping(address => bool) public isAllowedToSend;
 
+    mapping(address => bool) public guardians;
+    address payable nextOwner;
+    mapping(address => mapping(address => bool)) nextOwnerGuardianVotedBool;
+    uint guardiansResetCount;
+    uint public constant confirmationsFromGUardiansForReset = 3;
+
     constructor() {
         owner = payable(msg.sender);
+    }
+
+    function setGuardian(address _guardian, bool _isGuardian) public {
+        require(msg.sender == owner, "You are not the owner, aborting");
+        guardians[_guardian] = _isGuardian;
+    }
+
+    function proposeNewOwner(address payable _newOwner) public {
+        require(guardians[msg.sender], "You are not guardian of this wallet, aborting");
+        require(nextOwnerGuardianVotedBool[_newOwner][msg.sender] == false, "You already voted, aborting");
+        if (_newOwner != nextOwner) {
+            nextOwner = _newOwner;
+            guardiansResetCount = 0;
+        }
+
+        guardiansResetCount++;
+
+        if (guardiansResetCount >= confirmationsFromGUardiansForReset) {
+            owner = nextOwner;
+            nextOwner = payable(address(0));
+        }
     }
 
     function setAllowance(address _for, uint _amount) public {
